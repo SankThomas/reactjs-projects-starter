a25cf2285901c6bedb2581ffee3e45013e6fe2dc
Thomas Sankara <thomassankara74@gmail.com>
2022-08-08 15:03:06 +0300

    github issues

---
+++ 18-github-issues/build/src/App.js
@@ -0,0 +1,16 @@
+import Issues from "./components/Issues"
+
+function App() {
+  return (
+    <>
+      <section>
+        <h1>Github Issues App</h1>
+        <br />
+
+        <Issues />
+      </section>
+    </>
+  )
+}
+
+export default App
---
+++ 18-github-issues/build/src/components/Issues.js
@@ -0,0 +1,36 @@
+import useFetch from "../hooks/useFetch"
+import { format } from "date-fns"
+
+export default function Issues() {
+  const issues = useFetch()
+  return (
+    <div className="issues-container">
+      {issues.map(
+        ({ title, number, state, user, created_at, html_url, labels }) => (
+          <article key={number}>
+            <div>
+              <p className={`${state === "open" && "open"}`}></p>
+            </div>
+            <div>
+              <a href={html_url}>{title} </a>
+              {labels.map((label, index) => (
+                <small
+                  key={index}
+                  style={{
+                    backgroundColor: `#${label.color}`,
+                  }}
+                >
+                  <a href={label.url}>{label.name}</a>
+                </small>
+              ))}
+              <p>
+                #{number} opened {format(new Date(created_at), "dd MMMM yyyy")}{" "}
+                by <a href={user.html_url}>{user.login}</a>
+              </p>
+            </div>
+          </article>
+        )
+      )}
+    </div>
+  )
+}
