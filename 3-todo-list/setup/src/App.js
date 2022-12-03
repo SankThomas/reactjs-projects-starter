6ae96c8f8e95652f34301b6d050d2d085d65fd3c
Thomas Sankara <thomassankara74@gmail.com>
2022-12-03 12:26:25 +0300

    add UI + functionality + refactor code

--- 3-todo-list/setup/src/App.js
+++ 3-todo-list/setup/src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import List from "./components/List";
 
-function App() {
+const fetchLocalStorage = () => {
+  let items = localStorage.getItem("items");
+
+  if (items) {
+    return JSON.parse(localStorage.getItem("items"));
+  } else {
+    return [];
+  }
+};
+
+export default function App() {
+  const [text, setText] = useState("");
+  const [items, setItems] = useState(fetchLocalStorage());
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!text) {
+      alert("Input is empty");
+    } else {
+      const newItem = {
+        title: text,
+        id: new Date().getTime().toString(),
+      };
+      setItems([newItem, ...items]);
+      setText("");
+    }
+  };
+
+  const handleDelete = (id) => {
+    setItems(items.filter((item) => item.id !== id));
+  };
+
+  const handleEdit = (id) => {
+    const editingItem = items.find((item) => item.id === id);
+    setItems(items.filter((item) => item.id !== id));
+    setIsEditing(true);
+    setText(editingItem.title);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("items", JSON.stringify(items));
+  }, [items]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section>
+        <div className="container">
+          <h2>Todo List</h2>
+          <p className="item-number">
+            You have {items.length} items in your ToDo List
+          </p>
+
+          <div>
+            <form onSubmit={handleSubmit}>
+              <input
+                type="text"
+                name="todo-item"
+                id="todo-item"
+                placeholder="E.g. bread"
+                required
+                value={text}
+                onChange={(event) => setText(event.target.value)}
+              />
+              <button type="submit" onClick={handleSubmit}>
+                Add
+              </button>
+            </form>
+          </div>
+        </div>
+
+        {/* List items */}
+        <div className="list-items">
+          <List
+            items={items}
+            handleDelete={handleDelete}
+            handleEdit={handleEdit}
+          />
+        </div>
+      </section>
+    </>
   );
 }
-
-export default App;
