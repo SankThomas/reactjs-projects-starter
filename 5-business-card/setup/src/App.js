08e6b3c8a7ecc5e9020f5e5b26e3e46dce2eb818
sankthomas <thomassankara74@gmail.com>
2024-01-15 17:26:52 +0300

    add state values and click functionality

--- 5-business-card/setup/src/App.js
+++ 5-business-card/setup/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+import DefaultView from "./components/DefaultView";
+import Back from "./components/Back";
+
+export default function App() {
+  const [isShowing, setIsShowing] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section
+      style={{
+        maxWidth: 500,
+        marginInline: "auto",
+      }}
+    >
+      {isShowing ? (
+        <Back setIsShowing={setIsShowing} />
+      ) : (
+        <DefaultView setIsShowing={setIsShowing} />
+      )}
+    </section>
   );
 }
-
-export default App;
