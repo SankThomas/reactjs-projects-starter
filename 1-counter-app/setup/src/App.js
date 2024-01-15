5afa7e8dbe579aa3554d9b084fce240389f0a56e
sankthomas <thomassankara74@gmail.com>
2024-01-15 17:22:01 +0300

    counter app - complete

--- 1-counter-app/setup/src/App.js
+++ 1-counter-app/setup/src/App.js
@@ -3,28 +3,30 @@ import React, { useState } from "react";
 export default function App() {
   const [number, setNumber] = useState(0);
 
-  const increment = () => setNumber(number + 1);
+  console.log(number);
 
-  const decrement = () => setNumber(number - 1);
+  // function addValue() {
+  //   setNumber(number + 23);
+  // }
+
+  const addValue = () => {
+    setNumber(number + 23);
+  };
+
+  function deductValue() {
+    setNumber(number - 23);
+
+    if (number <= 0) {
+      alert("Number is already less than 0");
+      setNumber(0);
+    }
+  }
 
   return (
-    <div>
-      <article>
-        <ul>
-          <li>
-            <button onClick={increment}>+1</button>
-          </li>
-          <li>{number}</li>
-          <li>
-            <button onClick={decrement}>-1</button>
-          </li>
-        </ul>
-      </article>
+    <div className="container">
+      <button onClick={() => setNumber(number + 45)}>Add</button>
+      <h2>{number}</h2>
+      <button onClick={deductValue}>Subtract</button>
     </div>
   );
 }
-
-/* 
-1. Stateless component - class / function
-2. Stateful component - class / function
-*/
