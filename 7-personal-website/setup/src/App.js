aa522f6e7a92fa912a73f1059a55087d4bac1a15
sankthomas <thomassankara74@gmail.com>
2024-01-15 17:31:03 +0300

    minimalistic personal website

--- 7-personal-website/setup/src/App.js
+++ 7-personal-website/setup/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import phone from "./phone.png";
+import {
+  AiFillBehanceCircle,
+  AiFillDribbbleCircle,
+  AiFillInstagram,
+} from "react-icons/ai";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <h1>
+        Thomas Sankara is a Front-end Web Developer based in Nairobi, Kenya.
+      </h1>
+
+      <img src={phone} alt="phone" />
+
+      <h2>Hire me</h2>
+      <ul>
+        <li>
+          <AiFillBehanceCircle />
+        </li>
+        <li>
+          <AiFillDribbbleCircle />
+        </li>
+        <li>
+          <AiFillInstagram />
+        </li>
+      </ul>
+    </section>
   );
 }
-
-export default App;
