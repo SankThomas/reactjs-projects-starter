3452189a111d71c96c6596bb0c61bd2949cce6c5
sankthomas <thomassankara74@gmail.com>
2024-01-15 17:29:43 +0300

    state for generating passwords

---
+++ 8-password-generator/setup/src/data.js
@@ -0,0 +1,6 @@
+const numbers = "0123456789";
+const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const specialSymbols = "~`!@#$%^&*()_+-=|{}[]';:?/<>";
+
+export { numbers, lowercaseLetters, uppercaseLetters, specialSymbols };
