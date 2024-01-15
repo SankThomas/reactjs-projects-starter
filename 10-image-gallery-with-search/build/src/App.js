03503815da6a3184eb785c44bb2b09b560701e35
sankthomas <thomassankara74@gmail.com>
2024-01-15 17:23:47 +0300

    fetch and display photos and add search functionality

--- 10-image-gallery-with-search/build/src/App.js
+++ 10-image-gallery-with-search/build/src/App.js
@@ -1,20 +1,20 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 
 export default function App() {
-  const [photos, setPhotos] = useState([])
-  const [query, setQuery] = useState("landscape")
+  const [photos, setPhotos] = useState([]);
+  const [query, setQuery] = useState("landscape");
 
   useEffect(() => {
     const fetchPhotos = async () => {
       const res = await fetch(
         `https://api.unsplash.com/search/photos?client_id=${process.env.REACT_APP_UNSPLASH_API_KEY}&query=${query}`
-      )
-      const data = await res.json()
-      setPhotos(data.results)
-    }
+      );
+      const data = await res.json();
+      setPhotos(data.results);
+    };
 
-    fetchPhotos()
-  }, [query])
+    fetchPhotos();
+  }, [query]);
 
   return (
     <>
@@ -35,31 +35,39 @@ export default function App() {
         <br />
         <h2>Showing images about "{query}"</h2>
         <br />
-        <div className="photos-container">
-          {photos.map((photo) => (
-            <article key={photo.id}>
-              <img src={photo.urls.full} alt={photo.user.username} />
-              <div>
+        {photos ? (
+          <div className="photos-container">
+            {photos.map((photo) => (
+              <article key={photo.id}>
                 <img
-                  src={photo.user.profile_image.large}
+                  src={photo.urls.full}
                   alt={photo.user.username}
-                  className="avatar"
+                  loading="lazy"
                 />
-                <ul>
-                  <li>
-                    <a href={photo.user.portfolio_url}>{photo.user.name}</a>
-                  </li>
-                  <li>
-                    <small
-                      dangerouslySetInnerHTML={{ __html: photo.user.bio }}
-                    ></small>
-                  </li>
-                </ul>
-              </div>
-            </article>
-          ))}
-        </div>
+                <div>
+                  <img
+                    src={photo.user.profile_image.large}
+                    alt={photo.user.username}
+                    className="avatar"
+                  />
+                  <ul>
+                    <li>
+                      <a href={photo.user.portfolio_url}>{photo.user.name}</a>
+                    </li>
+                    <li>
+                      <small
+                        dangerouslySetInnerHTML={{ __html: photo.user.bio }}
+                      ></small>
+                    </li>
+                  </ul>
+                </div>
+              </article>
+            ))}
+          </div>
+        ) : (
+          <h2>Failed to fetch</h2>
+        )}
       </div>
     </>
-  )
+  );
 }
